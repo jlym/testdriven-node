4395c95b7b0400f1dc01da4af1ad4f637f884ec3
Jeffrey Lym <jeffreyalym@gmail.com>
2017-12-03 11:33:46 -0800

    Changed the type of the ID column of the users table from UUID to TEXT

--- users-db/create-db.sql
+++ users-db/create-db.sql
@@ -2,10 +2,8 @@
 CREATE DATABASE users;
 \c users
 
--- pgcrypto is required for the gen_random_uuid function.
-CREATE EXTENSION pgcrypto;
 CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id TEXT PRIMARY KEY,
     username TEXT NOT NULL UNIQUE,
     email TEXT NOT NULL UNIQUE,
     active BOOLEAN  NOT NULL,
